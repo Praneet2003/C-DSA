414914f243a698913a06e90a05c7ecb87a24cf40
Praneet2003 <rajpraneet4@gmail.com>
2024-06-14 07:49:28 +0530

    Triangulare star pattern printing

---
+++ Day3/Star pattern 1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,row=1;
+    cin>>n;
+    while(row<=n){
+        int col=1;
+        while(col<=row){//No of row's ke equal print krna hai *, col ke andar.
+            cout<<"* ";// ye while loop no of rows pe depend kr rha hai.
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+}
+// /tmp/PhO3s30FsW.o
+// 5
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+
+
+// === Code Execution Successful ===
