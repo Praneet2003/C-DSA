5bf204802d9e05961e5acad3df080d04c1c92489
Praneet2003 <rajpraneet4@gmail.com>
2024-06-16 16:31:12 +0530

    While loop in C++

---
+++ Day2/while loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 0;
+    while(i<=n){
+        cout<<i<<endl;
+        i++;
+    }
+}
+//Output: 
+// /tmp/aF1x9bEMtp.o
+// 5
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+
+
+// === Code Execution Successful ===
