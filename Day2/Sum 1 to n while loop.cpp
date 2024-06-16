11538c09b532380fe3e5f41ac037779cdcf91063
Praneet2003 <rajpraneet4@gmail.com>
2024-06-16 16:29:49 +0530

    Sum of numbers 1 to n

---
+++ Day2/Sum 1 to n while loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 0;
+    int sum = 0;
+    while(i<=n){
+        sum+=i;
+        i++;
+    }
+    cout<<sum;
+}
+// /tmp/m077RFO7g4.o
+// 10
+// 55
+
+// === Code Execution Successful ===
