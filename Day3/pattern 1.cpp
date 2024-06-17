8cd4ccb1fd2ca97931d8c2813d679dbe94fcb3f8
Praneet2003 <rajpraneet4@gmail.com>
2024-06-17 23:18:35 +0530

    number pattern

---
+++ Day3/pattern 1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i =0;
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    while(i<=n-1){
+        int j = 1;
+        while(j<=n){
+            cout<<j;
+            j++;
+        }
+    cout<<endl;
+    i++;
+    }
+    return 0;
+}
+// /tmp/msJsq5O0kw.o
+// enter the value of n: 6
+// 123456
+// 123456
+// 123456
+// 123456
+// 123456
+// 123456
+
+
+// === Code Execution Successful ===
