4510ad74c20c9fc7a42ca4767130b5c11be57272
Praneet2003 <rajpraneet4@gmail.com>
2024-06-21 20:22:25 +0530

    Increment & Decrement Operator

---
+++ Day5/Increment & Decrement Operator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i =7; 
+    int i1 = 10;
+    cout<<"Pre-increment : "<<++i<<endl;
+    cout<<"Post-increment : "<<i++<<endl;
+    cout<<"Post-Decrement : "<<i1--<<endl;
+    cout<<"Pre-Decrement : "<<--i1<<endl;
+    int a =10,b=1;
+    if(++a){
+         cout<<b;
+    }else{
+        cout<<++b;
+    }
+}
+// /tmp/9ZGTVvuq8v.o
+// Pre-increment : 8
+// Post-increment : 8
+// Post-Decrement : 10
+// Pre-Decrement : 8
+// 1
+
+// === Code Execution Successful ===
