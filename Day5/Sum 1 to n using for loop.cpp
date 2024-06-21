1df7a0d499409bc0885929cd08da68cd700ca4d8
Praneet2003 <rajpraneet4@gmail.com>
2024-06-21 20:23:26 +0530

    Sum 1 to n

---
+++ Day5/Sum 1 to n using for loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    int sum =0;
+    for(int i =1;i<=n;i++){
+        sum+=i;
+    }
+    cout<<"sum of numbers is: "<<sum<<endl;
+}
+// /tmp/oXRD73Vmp9.o
+// enter the value of n: 4
+// sum of numbers is: 10
+
+
+// === Code Execution Successful ===
