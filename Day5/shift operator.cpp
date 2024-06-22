1bedec32fccfd99e2dd933d9e77334d4b64ae091
Praneet2003 <rajpraneet4@gmail.com>
2024-06-22 22:19:25 +0530

    Shift Operator

---
+++ Day5/shift operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<(17>>1)<<endl;
+cout<<(17>>2)<<endl;
+cout<<(19<<1)<<endl;
+cout<<(21<<2)<<endl;
+}
+// /tmp/C8bw7QBLcx.o
+// 8
+// 4
+// 38
+// 84
+
+
+// === Code Execution Successful ===
