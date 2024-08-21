17a5cb6bcb16295627799338dd35a5fb17739def
Praneet2003 <rajpraneet4@gmail.com>
2024-08-22 00:01:45 +0530

    Finding length of an array

---
+++ Day9/finding length of array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int array[]={};
+    int size = sizeof(array)/sizeof(int);
+    //size of array is 4*15 &size of int is 4......
+    cout<<"size of array is: "<<size;
+}
+// /tmp/aneASeAoht.o
+// size of array is: 15
+
+// === Code Execution Successful ===
