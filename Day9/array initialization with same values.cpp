7dc82e7792a864460eb4693604e563650129a1c8
Praneet2003 <rajpraneet4@gmail.com>
2024-08-23 00:05:30 +0530

    Array initialization

---
+++ Day9/array initialization with same values.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int array[10];
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        array[i]={1};
+        cout<<array[i]<<" ";        
+    }
+}
+// /tmp/sUGAvwkEX0.o
+// Enter the value of n: 10
+// 1 1 1 1 1 1 1 1 1 1 
+
+// === Code Execution Successful ===
