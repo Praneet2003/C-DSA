d7b0789015460e0cf9a953901ec8545b3b48e576
Praneet2003 <rajpraneet4@gmail.com>
2024-08-24 23:46:08 +0530

    Binary search with range constraints

---
+++ Day10/Binary search without any error in range.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int BinarySearch(int arr[],int size,int key){
+    int start=0;
+    int end=size-1;
+    // this below formula will never exceed the range of int.
+    int mid=start+(end-start)/2;
+    while(start<=end){
+        if(arr[mid]==key){
+            return mid;
+        }
+        if(key>arr[mid]){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+        // this below formula will never exceed the range of int.
+        mid = start+(end-start)/2;
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int num[10];
+    cout<<"Enter the value's of array in Ascending Order: "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>num[i];
+    }
+    int key;
+    cout<<"Enter the value of key: "<<endl;
+    cin>>key;
+    int ans = BinarySearch(num,n,key);
+    if(ans){
+        cout<<"Congratulations! We Found the key at"<<ans<<endl;
+    }else{
+        cout<<"We were unable to Find the key..........";
+    }
+}
