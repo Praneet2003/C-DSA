04cec25ff6e90eb019ca8482886228287da67a0a
Praneet2003 <rajpraneet4@gmail.com>
2024-08-30 23:40:03 +0530

    char array

---
+++ Day13/char array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char name[10];
+    
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Hi ! your Name is: "<<name;
+}
+// /tmp/MDdl4IqSz4.o
+// Enter your name: Praneet
+// Hi ! your Name is: Praneet(\0) 
+// === Code Execution Successful ===
