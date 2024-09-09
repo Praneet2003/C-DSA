b51ef24d415f041dd98aca45f7588fc50cffeac6
Praneet2003 <rajpraneet4@gmail.com>
2024-09-09 23:17:33 +0530

    Character array I/O

---
+++ Day17/char array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //input of character array.
+    char arr[n+1];
+    cin>>arr;
+    int i=0;
+    //output of character array.
+    while(arr[i]!='\0'){
+        cout<<arr[i];
+        i++;
+    }
+}
+// /tmp/UpqXYRVRCt.o
+// 7
+// Praneet
+// Praneet
+
+// === Code Execution Successful ===
