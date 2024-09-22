cb4b0bfe04f72f26f3cf4a5595f43c5cf56b4c54
Praneet2003 <rajpraneet4@gmail.com>
2024-09-22 23:39:32 +0530

    Linked List All INsertions

---
+++ Practice/LInked_list.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int n){
+        this->data=n;
+        this->next=nullptr;
+    }
+};
+class LinkedList{
+    private:
+    Node* head;
+    public:
+    LinkedList(){
+        head = nullptr;
+    }
+    void insertAthead(int n){
+        Node *newnode = new Node(n);
+        
+        newnode->next=head;
+        head = newnode;
+    }
+    void insertAtEnd(int n){
+        Node *newnode = new Node(n);
+        if(head==nullptr){
+            head = newnode;
+            // newnode->next=head;
+        }else{
+            Node*temp =head;
+            while(temp->next!=nullptr){
+                temp=temp->next;
+            }
+            temp->next = newnode;
+            // newnode->next=nullptr;
+        }
+    }
+    void insertAtpos(int n,int pos){
+        Node *newnode = new Node(n);
+        if(pos==1){
+            newnode->next=head;
+            head = newnode;
+            return;
+        }else{
+            Node* temp=head;
+            for(int i=0;i<pos-1 && temp!=nullptr;i++){
+                temp=temp->next;
+            }
+            if(temp==nullptr){
+                cout<<"Position out of bounds"<<endl;
+                return;
+            }
+            newnode->next=temp->next;
+            temp->next=newnode;
+        }
+
+    }
+    void display(){
+        Node *temp = head;
+        while(temp!=nullptr){
+            cout<< temp->data<<" -> ";
+            temp=temp->next;
+        }
+    }
+};
+int main(){
+    int n,a,b,pos,c;
+    LinkedList obj;
+    cin>>n;
+    // int a;
+    
+    for(int i=0;i<n;i++){
+        cin>>a;
+        // cout<<a<<" ";
+        obj.insertAtEnd(a);
+    }
+    cin>>b>>pos;
+    obj.insertAtpos(b,pos);
+    cin>>c;
+    obj.insertAthead(c);
+    obj.display();
+}
