7558f564554ac2700a370912127d42c4d1bad3b9
Praneet2003 <rajpraneet4@gmail.com>
2024-10-07 23:24:52 +0530

    String vs char array in null Character

---
+++ Day24/string_basic.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s="Praneet Raj";
+    // getline(cin,s);
+    char arr[20]={'p','r','a','\0','n','e','t',' ','R','a','j'};
+    // cin>>arr;
+    cout<<"The string value is: "<<s<<endl;
+    cout<<"The char array value is: "<<arr<<endl;
+// }
+// #include<iostream>
+// #include<cmath>
+
+// using namespace std;
+
+// int main()
+// {
+	char s1[]="hello world";	
+	string r="hello world";
+	
+	s[3]='\0';
+	r[3]='\0';
+	
+	cout<<s<<endl<<r;
+	//string does stop until the last value it prints the null character as well.
+	return 0;
+}
