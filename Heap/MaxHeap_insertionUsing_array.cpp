3be6b462fc116efa99f0d7f99b6bf5b3adfb9e42
Praneet2003 <rajpraneet4@gmail.com>
2024-11-21 22:03:33 +0530

    MaxHeap Insertion

---
+++ Heap/MaxHeap_insertionUsing_array.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class MaxHeap{
+    public:
+    int arr[100];
+    int size =0;
+    void insert(int val){
+        size = size+1;
+        int i=size;
+        arr[i]=val;
+        while(i>1){
+            int parent = i/2;
+            if(arr[parent]<arr[i]){
+                swap(arr[parent],arr[i]);
+                i=parent;
+            }else{
+                return;
+            }
+        }
+    }
+    void print(int n){
+        for(int i=1;i<=size;i++){
+            cout<<arr[i]<<" ";
+        }
+    }
+};
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    MaxHeap h;
+    for(int i=0;i<n;i++){
+        int d;
+        cin>>d;
+        h.insert(d);
+    }
+    h.print(n);
+}
