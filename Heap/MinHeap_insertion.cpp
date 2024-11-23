17541292eb3a0d8406f4127096ba82a95c48c197
Praneet2003 <rajpraneet4@gmail.com>
2024-11-23 22:33:45 +0530

    MinHeap insertion

---
+++ Heap/MinHeap_insertion.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class MINHeap{
+    public:
+    int arr[100];
+    int size = 0;
+    void insert(int val){
+        size++;
+        int i=size;
+        arr[size]=val;
+        while(i>1){
+            int parent = i/2;
+            if(arr[parent]>arr[i]){
+                swap(arr[parent],arr[i]);
+                i=parent;
+            }else{
+                return;
+            }
+        }
+    }
+    void print(){
+        for(int i=1;i<=size;i++){
+            cout<<arr[i]<<" ";
+        }
+    }
+};
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    MINHeap h;
+    for(int i=0;i<n;i++){
+        int d;
+        cin>>d;
+        h.insert(d);
+    }
+    h.print();
+}
