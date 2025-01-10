6bcf4c84624b59ae92271f7781d03a907d68383e
praneet raj <147131848+Praneet2003@users.noreply.github.com>
2025-01-10 16:25:00 +0530

    Linear probing hashing method

--- Hashing/linear_Probing.cpp
+++ Hashing/linear_Probing.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 int main(){
     int k,size = 10;
-    cin>>k;
+    cin>>k;// here k is the number of elements to be kept in hash table
     int hashtable[size];
     for(int i=0;i<size;i++){
-        hashtable[i]=-1;
+        hashtable[i]=-1;// empty hash table all are assigned as -1
     }
     for(int i=0;i<k;i++){
         int key;
         cin>>key;
         int index = (2*key+3)%size;
         if(hashtable[index]==-1){
-            hashtable[index]=key;
+            hashtable[index]=key;// agar space khali hai to key insert hoga
         }else{
             while(hashtable[index]!=-1){
-                index++;
-                if(index==size){
+                index++;// index empty na hone pr increment kr do
+                if(index==size){// agar increment hote hote table size ke equal aa jaye to index ko 0 assign kr do
                     index=0;
                 }
             }
